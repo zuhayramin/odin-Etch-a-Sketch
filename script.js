792cc9650c5496026ea15d9a03827247a3eb28f1
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-20 14:10:22 +0400

    Create basic structure of application

---
+++ script.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector("grid")
+// let gridNumber = 16
+
+// for (let row = 0; row < gridNumber; row++) {
+//     for (let column = 0; column < array.length; column++) {
+//         let box = document.createElement("div")
+//         grid.appendChild(box)
+//     }
+// }
