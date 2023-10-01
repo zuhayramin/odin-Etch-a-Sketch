6711557ee30424e2df1ff6d5ba1a941b8d63f860
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-10-01 19:32:31 +0400

    Add button to change size of grid

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const grid = document.querySelector(".grid")
+const changeSizeBtn = document.querySelector(".changeSizeBtn")
 let gridNumber = 32
 
-for (let column = 0; column < gridNumber; column++) {
-    let column = document.createElement("div")
-    column.classList = "column"
+changeSizeBtn.addEventListener("click", changeSize)
 
-    for (let row = 0; row < gridNumber; row++) {
-        let row = document.createElement("div")
-        row.classList = "row"
-        column.appendChild(row)
-        row.addEventListener("mouseover", function () {
-            row.style.cssText = "background-color: black;"
-        })
+function changeSize(gridNumber) {
+    let gridSize = 1000
+    while (gridSize > 100) {
+        gridSize = parseInt(
+            prompt(
+                "How many squares per side would you like? Enter a value between 1 and 100"
+            )
+        )
     }
-    grid.appendChild(column)
+    gridNumber = gridSize
+    printGrid(gridNumber)
 }
+
+function printGrid(gridNumber) {
+    for (let column = 0; column < gridNumber; column++) {
+        let column = document.createElement("div")
+        column.classList = "column"
+
+        for (let row = 0; row < gridNumber; row++) {
+            let row = document.createElement("div")
+            row.classList = "row"
+            column.appendChild(row)
+            row.addEventListener("mouseover", function () {
+                row.style.cssText = "background-color: black;"
+            })
+        }
+        grid.appendChild(column)
+    }
+}
+
+printGrid(gridNumber)
